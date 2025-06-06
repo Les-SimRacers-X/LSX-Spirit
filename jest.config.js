c511cb54f9ec375a3a30c5384a22f0a34319b13c
Zeikrom251 <ryanechikhi2004@gmail.com>
2025-06-06 15:16:46 +0200

    ♻️ chore: minimaliste jest config

--- jest.config.js
+++ jest.config.js
@@ -1,8 +1,3 @@
 module.exports = {
   testEnvironment: 'node',
-  collectCoverage: true,
-  coverageDirectory: 'coverage',
-  coverageReporters: ['text', 'lcov', 'html'],
-  testMatch: ['**/context/**/*.test.js', '**/context/**/*.spec.js'],
-  setupFilesAfterEnv: ['<rootDir>/context/tests/setup.js'],
 };
