d9700059bf656bf6cde0193319990cc6d63ce793
Zeikrom251 <ryanechikhi2004@gmail.com>
2025-06-07 16:23:34 +0200

    ✨ feat: updating commands

--- commands/profil.js
+++ commands/profil.js
@@ -5,20 +5,34 @@ const { errorHandler } = require('../context/utils/errorHandling');
 
 module.exports = {
   name: 'profil',
-  type: 'APPLICATION',
+  description:
+    'Cette commande vous permez de visualiser les licences des autres utilisateurs.',
+  dm: false,
+  utilisation: '<user>',
+  permission: 'Aucune',
+  options: [
+    {
+      type: 'user',
+      name: 'user',
+      description: 'Sélectionner un utilisateur',
+      required: false,
+    },
+  ],
 
   async run(bot, interaction) {
     try {
-      const userTarget = interaction.targetUser;
+      const targetUser = interaction.options.getUser('user');
 
-      const { embeds, components } = await licenceDisplay(userTarget.id);
+      let selectedUser = !targetUser ? interaction.user.id : targetUser.id;
+
+      const { embeds, components } = await licenceDisplay(selectedUser);
 
       await interaction.reply({
         embeds,
         ephemeral: true,
       });
     } catch (error) {
-      errorHandler(interaction, error);
+      await errorHandler(interaction, error);
     }
   },
 };
--- commands/send-sanction.js
+++ commands/send-sanction.js
@@ -1,5 +1,6 @@
-const Discord = require('discord.js');
+const { EmbedBuilder, ModalBuilder, TextInputBuilder, ActionRowBuilder } = require('discord.js');
 const { Config } = require('../context/config');
+const { emoteComposer } = require('../context/utils/utils');
 
 module.exports = {
   name: 'send-sanction',
@@ -7,10 +8,10 @@ module.exports = {
 
   async run(bot, interaction) {
     if (!interaction.member.roles.cache.has('1321919765140344895')) {
-      const embedNoPermissions = new Discord.EmbedBuilder()
+      const embedNoPermissions = new EmbedBuilder()
         .setColor(Config.colors.crossColor)
         .setDescription(
-          `${Config.emojis.crossEmoji} **Vous n'avez pas les permissions nécessaire pour utiliser cette command !**`
+          `### ${emoteComposer(Config.emotes.failure)} Vous n'avez pas les permissions nécessaire pour utiliser cette command !`
         );
 
       return interaction.reply({
@@ -20,28 +21,28 @@ module.exports = {
     } else {
       const user = interaction.targetUser;
       // Modal d'envoi de sanction
-      const modalSendingSanction = new Discord.ModalBuilder()
+      const modalSendingSanction = new ModalBuilder()
         .setCustomId(`sendSanction_${user.id}`)
         .setTitle('Envoi de sanction');
 
-      const modalMessageInput = new Discord.TextInputBuilder()
+      const modalMessageInput = new TextInputBuilder()
         .setCustomId(`messageContent`)
         .setLabel('Description de la sanction')
         .setPlaceholder('Exemple : Absence sur un événement...')
         .setRequired(true)
-        .setStyle(Discord.TextInputStyle.Paragraph);
+        .setStyle(TextInputStyle.Paragraph);
 
-      const modalPointToRemoveInput = new Discord.TextInputBuilder()
+      const modalPointToRemoveInput = new TextInputBuilder()
         .setCustomId(`sanctionPoints`)
         .setLabel('Points à retirer :')
         .setPlaceholder('Exemple : 5 ou rien aucun points à enlever')
-        .setStyle(Discord.TextInputStyle.Short);
+        .setStyle(TextInputStyle.Short);
 
       const modalMessageActionRow =
-        new Discord.ActionRowBuilder().addComponents(modalMessageInput);
+        new ActionRowBuilder().addComponents(modalMessageInput);
 
       const modalPointToRemoveActionRow =
-        new Discord.ActionRowBuilder().addComponents(modalPointToRemoveInput);
+        new ActionRowBuilder().addComponents(modalPointToRemoveInput);
 
       modalSendingSanction.addComponents(
         modalMessageActionRow,
