d43fbeb70a615da8cab34223b8dfe43b9f90062a
Zeikrom251 <ryanechikhi2004@gmail.com>
2025-07-12 16:35:40 +0200

    🐛 fix: correct way to get a user UUID, there was no condition before

--- context/utils/userGameUXID.js
+++ context/utils/userGameUXID.js
@@ -4,40 +4,42 @@ const {
   getProfileFromUserName,
 } = require('psn-api');
 const axios = require('axios');
+const { errorHandler } = require('./errorHandling');
 require('dotenv').config();
 
-async function getConsoleUXID(username) {
-  try {
-    const response = await axios.get(
-      `https://xbl.io/api/v2/search/${username}`,
-      {
-        headers: {
-          'X-Authorization': process.env.XBOX_APIKEY,
-        },
+async function getConsoleUXID(username, platform) {
+  if (platform === 'Playstation') {
+    try {
+      const accessCode = await exchangeNpssoForCode(process.env.PSN_APIKEY);
+      const accessToken = await exchangeCodeForAccessToken(accessCode);
+      const userProfile = await getProfileFromUserName(accessToken, username);
+
+      if (userProfile?.profile?.accountId) {
+        return { platform: 'P', id: userProfile.profile.accountId };
       }
-    );
-    if (response.data.people && response.data.people.length > 0) {
-      return { platform: 'M', id: response.data.people[0].xuid };
+    } catch (error) {
+      await errorHandler(_, error);
     }
-  } catch (error) {
-    console.log(
-      "Erreur lors de la récupération d'un console ID. PSN Sera TEST"
-    );
   }
 
-  try {
-    const accessCode = await exchangeNpssoForCode(process.env.PSN_APIKEY);
-    const accessToken = await exchangeCodeForAccessToken(accessCode);
-    const userProfile = await getProfileFromUserName(accessToken, username);
-
-    if (userProfile?.profile?.accountId) {
-      return { platform: 'P', id: userProfile.profile.accountId };
+  if (platform === 'Xbox') {
+    try {
+      const response = await axios.get(
+        `https://xbl.io/api/v2/search/${username}`,
+        {
+          headers: {
+            'X-Authorization': process.env.XBOX_APIKEY,
+          },
+        }
+      );
+      if (response.data.people && response.data.people.length > 0) {
+        return { platform: 'M', id: response.data.people[0].xuid };
+      }
+    } catch (error) {
+      await errorHandler(_, error);
     }
-  } catch (error) {
-    console.log("Erreur lors de la récupération de l'ID PSN");
   }
 
-  console.error("Aucun ID n'a été trouver");
   return undefined;
 }
 
